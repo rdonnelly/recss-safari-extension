7f5a2b31f38485e3367f835ee6f37cdcf335da4c
Ryan Donnelly <ryanjdonnelly@gmail.com>
2013-07-09 18:06:00 -0400

    initial code import

---
+++ recss.js
@@ -0,0 +1,11 @@
+var recss = function(){var i,a,s;a=document.getElementsByTagName('link');for(i=0;i<a.length;i++){s=a[i];if(s.rel.toLowerCase().indexOf('stylesheet')>=0&&s.href) {var h=s.href.replace(/(&|\?)forceReload=\d+/,'');s.href=h+(h.indexOf('?')>=0?'&':'?')+'forceReload='+(new Date().valueOf());}}};
+
+function handleMessage(msgEvent) {
+	var messageName = msgEvent.name;
+
+	if (messageName === 'recss') {
+		recss();
+	}
+}
+
+safari.self.addEventListener('message', handleMessage, false);
